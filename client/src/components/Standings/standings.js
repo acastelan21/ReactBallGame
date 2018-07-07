02840d07bc92791df33b66b2197eca51fb5894b0
AlCast <alvaro.castelan.21@gmail.com>
2018-07-07 17:21:41 -0500

    restructured standings component

--- client/src/components/Standings/standings.js
+++ client/src/components/Standings/standings.js
@@ -1,57 +1,134 @@
-import React from "react";
+import React, {Component} from "react";
+import API from "../../utils/API";
 
-const Standings = props => 
-    <div className="row">
-        <div className="col-6">
-            <div className="leagueName">National League
+class Standings extends Component{
+
+    constructor(){
+    super();
+this.state ={
+    standingsNLWest:[],
+    standingsNLCentral:[{}],
+    standingsNLEast:[],
+    standingsALEast:[],
+    standingsALCentral:[],
+    standingsALWest:[], 
+
+}
+}
+handleSearchStandings =(event)=>{
+    event.preventDefault();
+    API.SearchForStandings()
+    .then((response)=>{
+        console.log("=====================================");
+        console.log(response.data);
+        console.log("======================================");
+        
+        this.setState({
+        standingsNLWest:response.data.league.season.leagues[0].divisions[0].teams,
+        
+        standingsNLCentral:response.data.league.season.leagues[0].divisions[1].teams,
+        standingsNLEast:response.data.league.season.leagues[0].divisions[2].teams,
+        standingsALEast:response.data.league.season.leagues[1].divisions[0].teams,
+        standingsALCentral:response.data.league.season.leagues[1].divisions[1].teams,
+        standingsALWest:response.data.league.season.leagues[1].divisions[2].teams, 
+        });
+        
+    })
+}
+render(){
+    return(
+        
+            
+                <div className="sectionName"> Standings
+                <button onClick={this.handleSearchStandings} type="submit" className="btn btn-primary">
+                Get Standings
+                </button>
+                <div className="row">
+                <div className="col-6">
+                <div className="leagueName">National League
                 <div className="divisionName">West
-                    <div className="StandTeam">1.{props.standingsNLWestTeam1} W: {props.standingsNLWestTeam1W} L: {props.standingsNLWestTeam1L} GB: {props.standingsNLWestTeam1GB}</div>
-                    <div className="StandTeam">2.{props.standingsNLWestTeam2} W: {props.standingsNLWestTeam2W} L: {props.standingsNLWestTeam2L} GB: {props.standingsNLWestTeam2GB}</div>
-                    <div className="StandTeam">3.{props.standingsNLWestTeam3} W: {props.standingsNLWestTeam3W} L: {props.standingsNLWestTeam3L} GB: {props.standingsNLWestTeam3GB}</div>
-                    <div className="StandTeam">4.{props.standingsNLWestTeam4} W: {props.standingsNLWestTeam4W} L: {props.standingsNLWestTeam4L} GB: {props.standingsNLWestTeam4GB}</div>
-                    <div className="StandTeam">5.{props.standingsNLWestTeam5} W: {props.standingsNLWestTeam5W} L: {props.standingsNLWestTeam5L} GB: {props.standingsNLWestTeam5GB}</div>
+                
+                {this.state.standingsNLWest.map((standingsNLWest,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsNLWest.name} W: {standingsNLWest.win} L: {standingsNLWest.loss} GB : {standingsNLWest.games_back}
+                    </div>
                     
                 </div>
-                <div className="divisionName">Central
-                    <div className="StandTeam">1.</div>
-                    <div className="StandTeam">2.</div>
-                    <div className="StandTeam">3.</div>
-                    <div className="StandTeam">4.</div>
-                    <div className="StandTeam">5.</div>
+              
+                ))}
                 </div> 
-                <div className="divisionName">East
-                    <div className="StandTeam">1.</div>
-                    <div className="StandTeam">2.</div>
-                    <div className="StandTeam">3.</div>
-                    <div className="StandTeam">4.</div>
-                    <div className="StandTeam">5.</div>
+                <div className="divisionName">Central
+                {this.state.standingsNLCentral.map((standingsNLCentral,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsNLCentral.name} W: {standingsNLCentral.win} L: {standingsNLCentral.loss} GB : {standingsNLCentral.games_back}
+                    </div>
+                    
                 </div>
-            </div>
-        </div>
-        <div className="col-6">
-            <div className ="leagueName">American League
+              
+                ))}
+                </div>
+                <div className="divisionName">East
+                {this.state.standingsNLEast.map((standingsNLEast,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsNLEast.name} W: {standingsNLEast.win} L: {standingsNLEast.loss} GB : {standingsNLEast.games_back}
+                    </div>
+                    
+                </div>
+              
+                ))}
+                </div>
+                </div> 
+                </div>
+                <div className="col-6">
+                <div className="leagueName">American League 
                 <div className="divisionName">West
-                    <div className="StandTeam">1.</div>
-                    <div className="StandTeam">2.</div>
-                    <div className="StandTeam">3.</div>
-                    <div className="StandTeam">4.</div>
-                    <div className="StandTeam">5.</div>
+                {this.state.standingsALWest.map((standingsALWest,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsALWest.name} W: {standingsALWest.win} L: {standingsALWest.loss} GB : {standingsALWest.games_back}
+                    </div>
+                    
+                </div>
+              
+                ))}
                 </div>
                 <div className="divisionName">Central
-                    <div className="StandTeam">1.</div>
-                    <div className="StandTeam">2.</div>
-                    <div className="StandTeam">3.</div>
-                    <div className="StandTeam">4.</div>
-                    <div className="StandTeam">5.</div>
-                </div> 
-                <div className="divisionName">East
-                    <div className="StandTeam">1.</div>
-                    <div className="StandTeam">2.</div>
-                    <div className="StandTeam">3.</div>
-                    <div className="StandTeam">4.</div>
-                    <div className="StandTeam">5.</div>
+                {this.state.standingsALCentral.map((standingsALCentral,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsALCentral.name} W: {standingsALCentral.win} L: {standingsALCentral.loss} GB : {standingsALCentral.games_back}
+                    </div>
+                    
                 </div>
-            </div>
-        </div>
-    </div> //row
+              
+                ))}
+                </div>
+                <div className="divisionName">East
+                {this.state.standingsALEast.map((standingsALEast,i) =>(
+                <div key={i}>
+                
+                
+                    <div className="StandTeam" > {i + 1+"."}{standingsALEast.name} W: {standingsALEast.win} L: {standingsALEast.loss} GB : {standingsALEast.games_back}
+                    </div>
+                    
+                </div>
+              
+                ))}
+                </div>
+                </div> 
+                </div>
+                </div>
+                </div>//standings
+                
+                    )
+                }
+            }
 export default Standings;
--- client/src/pages/Members/members.js
+++ client/src/pages/Members/members.js
@@ -1,84 +1,21 @@
 import React, {Component} from "react"; 
 import NavBar from "../../components/NavBar";
-import API from "../../utils/API";
 import Standings from "../../components/Standings";
-class Members extends Component{
-    constructor(){
-        super();
-        this.state ={
-            standingsNLWest:[{},{},{},{},{}],
-            standingsNLCentral:[{}],
-            standingsNLEast:[{}],
-            standingsALEast:[{}],
-            standingsALCentral:[{}],
-            standingsALWest:[{}], 
-    
-        }
-        }
 
-    handleSearchStandings =(event)=>{
-        event.preventDefault();
-        API.SearchForStandings()
-        .then((response)=>{
-            console.log("=====================================");
-            console.log(response.data);
-            console.log("======================================");
-            
-            this.setState({
-            standingsNLWest:response.data.league.season.leagues[0].divisions[0].teams,
-            
-            standingsNLCentral:response.data.league.season.leagues[0].divisions[1].teams,
-            standingsNLEast:response.data.league.season.leagues[0].divisions[2].teams,
-            standingsALEast:response.data.league.season.leagues[1].divisions[0].teams,
-            standingsALCentral:response.data.league.season.leagues[1].divisions[1].teams,
-            standingsALWest:response.data.league.season.leagues[1].divisions[2].teams, 
-            });
-            console.log(this.state.standingsNLWest);
-            console.log(this.state.standingsNLCentral);
-            console.log(this.state.standingsNLEast);
-            console.log(this.state.standingsALEast);
-            console.log(this.state.standingsALCentral);
-            console.log(this.state.standingsALWest);
-        })
-    }
-    
+class Members extends Component{
+  
     render(){
         return(
             <div className="membersComp">
                 <NavBar/>
-                <div>Members Page</div>
                 <div className="row">
-                    <div className="col-6">Standings
-                    <button onClick={this.handleSearchStandings} type="submit" className="btn btn-primary">
-                    Get Standings
-                    </button>
-                    <Standings
-                    standingsNLWestTeam1={this.state.standingsNLWest[0].name}
-                    standingsNLWestTeam1W={this.state.standingsNLWest[0].win}
-                    standingsNLWestTeam1L={this.state.standingsNLWest[0].loss}
-                    standingsNLWestTeam1GB={this.state.standingsNLWest[0].games_back}
-
-                    standingsNLWestTeam2={this.state.standingsNLWest[1].name}
-                    standingsNLWestTeam2W={this.state.standingsNLWest[1].win}
-                    standingsNLWestTeam2L={this.state.standingsNLWest[1].loss}
-                    standingsNLWestTeam2GB={this.state.standingsNLWest[1].games_back}
-
-                    standingsNLWestTeam3={this.state.standingsNLWest[2].name}
-                    standingsNLWestTeam3W={this.state.standingsNLWest[2].win}
-                    standingsNLWestTeam3L={this.state.standingsNLWest[2].loss}
-                    standingsNLWestTeam3GB={this.state.standingsNLWest[2].games_back}
-
-                    standingsNLWestTeam4={this.state.standingsNLWest[3].name}
-                    standingsNLWestTeam4W={this.state.standingsNLWest[3].win}
-                    standingsNLWestTeam4L={this.state.standingsNLWest[3].loss}
-                    standingsNLWestTeam4GB={this.state.standingsNLWest[3].games_back}
-
-                    standingsNLWestTeam5={this.state.standingsNLWest[4].name}
-                    standingsNLWestTeam5W={this.state.standingsNLWest[4].win}
-                    standingsNLWestTeam5L={this.state.standingsNLWest[4].loss}
-                    standingsNLWestTeam5GB={this.state.standingsNLWest[4].games_back}
-                    />
-                    
+                    <div className="col-12">
+                        <div>Members Page</div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                    <Standings/>
                     </div>
                     <div className="col-3">News</div>
                     <div className="col-3">Roster</div>
