647b112582e60079e3f212001dcd4dd217b4817e
AlCast <alvaro.castelan.21@gmail.com>
2018-07-11 20:39:50 -0500

    added options to questions

--- client/src/components/Questions/questions.js
+++ client/src/components/Questions/questions.js
@@ -10,6 +10,7 @@ const Questions = props =>
                     <div className = "q1">
                         <p>1. Who will win the game?</p>
                             <select name="q1" id="Q1" value={props.value} onChange={props.handleChange}>
+                                <option value="null"></option>
                                 <option value={props.homeTeamAbbr}>{props.homeTeamAbbr}</option>
                                 <option value={props.awayTeamAbbr}>{props.awayTeamAbbr}</option>
                             </select>
@@ -17,6 +18,7 @@ const Questions = props =>
                     <div className = "q2">
                         <p>2. Will {props.homeTeamProbablePitcherLastName} have more than 5 strikeouts today?</p>
                             <select name= "q2"id="Q2" value={props.value} onChange={props.handleChange}>
+                                <option value="null"></option>
                                 <option value="yes">Yes</option>
                                 <option value="no">No</option>
                             </select> 
@@ -24,6 +26,7 @@ const Questions = props =>
                     <div className = "q3">
                         <p>3.How many hits will {props.awayTeamAbbr} </p>
                             <select name="q3"id="Q3" value={props.value} onChange={props.handleChange}>
+                                <option value="null"></option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -38,6 +41,7 @@ const Questions = props =>
                     <div className = "q4">
                         <p>4.Will {props.homeTeamAbbr} score seven or more runs? </p>
                             <select name="q4"id="Q4" value={props.value} onChange={props.handleChange}>
+                                <option value="null"></option>
                                 <option value="yes">Yes</option>
                                 <option value="no">No</option>
                             </select>
@@ -45,6 +49,7 @@ const Questions = props =>
                     <div className = "q5">
                     <p>5.Will {props.awayTeamAbbr} have more than 2 errors in today's game? </p>
                             <select name="q5" id="Q5" value={props.value} onChange={props.handleChange}>
+                                <option value="null"></option>
                                 <option value="yes">Yes</option>
                                 <option value="no">No</option>
                             </select>
